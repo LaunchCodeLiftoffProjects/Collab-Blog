32aefdb69c3b6fb6b7b232813a9d314879294386
John Woolbright <jwoolbright23@gmail.com>
2020-08-10 14:59:14 -0500

    front-end-service

--- blog/src/main/java/org/launchcode/blog/Blogs/BlogRepository.java
+++ blog/src/main/java/org/launchcode/blog/Blogs/BlogRepository.java
@@ -1,6 +1,8 @@
 package org.launchcode.blog.Blogs;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200")
 public interface BlogRepository extends JpaRepository<Blog, Long> {
 }
--- blog/src/test/java/org/launchcode/blog/BlogTests/BlogTests.java
+++ blog/src/test/java/org/launchcode/blog/BlogTests/BlogTests.java
@@ -1,10 +1,15 @@
 package org.launchcode.blog.BlogTests;
 
 import org.junit.jupiter.api.Test;
+import org.launchcode.blog.Blogs.Blog;
+import org.launchcode.blog.Blogs.BlogRepository;
 import org.launchcode.blog.TestsInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.Date;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -12,13 +17,24 @@ import static org.hamcrest.Matchers.*;
 
 @TestsInterface
 public class BlogTests {
+
+	@Autowired
+	private BlogRepository blogRepository;
 	
 	@Autowired
 	private MockMvc mockMvc;
 	
 	@Autowired
 	private BlogTestsUtil blogTestsUtil;
-	
+
+
+	@Test
+	public void fillDatabaseWithBlogs() {
+		Blog blog1 = new Blog("New Header", "New Subheader", "New Image", "New Body", new Date());
+		Blog blog2 = new Blog("Second Header", "New Second Header", "Second image", "Second Body", new Date());
+		blogRepository.save(blog1);
+		blogRepository.save(blog2);
+	}
 	@Test
 	public void getBlogs_ReturnsListOfBlogs() throws Exception {
 		blogTestsUtil.tearDown();
