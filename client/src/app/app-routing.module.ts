f1edacbb0d2a5334d129d987d3f959f433cd0b7c
Jonathan Cokely <jon.cokely@gmail.com>
2022-01-20 19:50:41 -0600

    Created view-blog component for issue 27

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { AddBlogComponent } from './add-blog/add-blog.component';
 import { BlogListComponent } from './blog-list/blog-list.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { ViewBlogComponent } from './view-blog/view-blog.component';
 
 const routes: Routes = [
   {path: "", component: WelcomePageComponent},
   {path: "blog-list", component: BlogListComponent },
   {path: "add-blog", component: AddBlogComponent },
   {path: "welcome", component: WelcomePageComponent },
+  {path: "view-blog", component: ViewBlogComponent}
 ];
 
 @NgModule({
---
+++ client/src/app/view-blog/view-blog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-blog',
+  templateUrl: './view-blog.component.html',
+  styleUrls: ['./view-blog.component.css']
+})
+export class ViewBlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
